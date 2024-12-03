d9891e59c1d1758b89aa6c0c8bfe9911d3d3d43d
Giuliana Cestorame <giulianaacestorame@gmail.com>
2024-12-03 16:44:31 +0000

    funcionalidad completa modulo ventas

--- Avivas/app/controllers/ventas_controller.rb
+++ Avivas/app/controllers/ventas_controller.rb
@@ -5,37 +5,57 @@ class VentasController < ApplicationController
 
   def new
     @venta = Venta.new
+    @productos_con_stock = Producto.where('stock > 0') 
     @venta.ventas_productos.build
   end
 
   def create
     ActiveRecord::Base.transaction do
-      if params[:venta][:cliente_id] == "new"
+      # Verificar si el cliente es nuevo
+      if params[:venta][:cliente_id].blank?
         @cliente = Cliente.create!(cliente_params)
         params[:venta][:cliente_id] = @cliente.id
+        logger.info "Nuevo cliente creado: #{@cliente.inspect}"
       else
         # Eliminar los parámetros de cliente si no se está creando uno nuevo
         params[:venta].delete(:nombre)
         params[:venta].delete(:apellido)
         params[:venta].delete(:dni)
         params[:venta].delete(:telefono)
+        logger.info "Cliente existente seleccionado: #{params[:venta][:cliente_id]}"
       end
-
+  
       @venta = Venta.new(venta_params)
+      #calcula total
+      @venta.total = @venta.ventas_productos.sum { |vp| vp.cantidad * vp.precio_venta }
+      logger.info "Total calculado: #{@venta.total}"
+  
       if @venta.save
+        # actualizar el stock de los productos vendidos
         @venta.ventas_productos.each do |vp|
           producto = vp.producto
+          logger.info "Actualizando stock de producto: #{producto.nombre}, stock actual: #{producto.stock}"
           producto.update!(stock: producto.stock - vp.cantidad)
         end
-        redirect_to ventas_path, notice: 'Venta registrada con éxito.'
+        flash[:notice] = "Venta registrada con éxito."
+        redirect_to ventas_path
       else
-        raise ActiveRecord::Rollback
+        logger.error "Errores al guardar la venta: #{@venta.errors.full_messages.join(', ')}"
+        flash[:alert] = @venta.errors.full_messages.join(', ')
+        raise ActiveRecord::Rollback # no se guarda nada si hay error
       end
     end
   rescue ActiveRecord::RecordInvalid => e
     flash.now[:alert] = e.message
     render :new
+  rescue => e
+    flash.now[:alert] = "Error inesperado: #{e.message}"
+    logger.error "Error al crear la venta: #{e.message}"
+    logger.error e.backtrace.join("\n")
+    render :new
   end
+  
+  
 
   def cancel
     puts("estoy en el controlador de cancelar venta ")
@@ -56,12 +76,13 @@ class VentasController < ApplicationController
   private
 
   def venta_params
-    params.require(:venta).permit(:fecha_hora, :total, :user_id, :cliente_id, ventas_productos_attributes: [:id, :producto_id, :cantidad, :precio_venta, :_destroy])
+    params.require(:venta).permit(:cliente_id, :user_id, :fecha_hora, ventas_productos_attributes: [:producto_id, :cantidad, :precio_venta, :_destroy])
   end
 
   def cliente_params
     params.require(:venta).permit(:nombre, :apellido, :dni, :telefono)
   end
 
-  
+
+
 end
--- Avivas/app/models/venta.rb
+++ Avivas/app/models/venta.rb
@@ -5,17 +5,15 @@ class Venta < ApplicationRecord
   has_many :ventas_productos, dependent: :destroy
   has_many :productos, through: :ventas_productos
 
-  validates :fecha_hora, :total, :user_id, presence: true
   accepts_nested_attributes_for :ventas_productos, allow_destroy: true
 
-  before_create :calculate_total
+  validates :fecha_hora, :total, :user_id, presence: true
+
   before_create :check_stock
 
   private
 
-  def calculate_total
-    self.total = ventas_productos.sum { |vp| vp.cantidad * vp.precio_venta }
-  end
+
 
   def check_stock
     ventas_productos.each do |vp|
